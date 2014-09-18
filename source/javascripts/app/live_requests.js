d44e38db9e66dfbe0b4be516a7e85666b152a4ef
klacointe <kevinlacointe@gmail.com>
2014-09-18 10:58:55 +0200

    Perform live requests on API.
    Breaks tocify because of dynamic contents.
    
    https://github.com/gfranko/jquery.tocify.js/issues/56

---
+++ source/javascripts/app/live_requests.js
@@ -0,0 +1,30 @@
+$(function() {
+
+  // Config
+  config = {
+    api_key: 'demo',
+    host: window.location.protocol + '//' + window.location.host.replace('doc.', 'api.')
+  }
+  // End Config
+
+  function urlWithHost(path) {
+    return config.host + path;
+  }
+
+  function makeRequest(e) {
+    $.ajax({
+      url: urlWithHost( $(e).data('path') ),
+      data: { api_key: config.api_key },
+      xhrFields: { withCredentials: true }
+    }).success(function(resp) {
+      $(e).text( JSON.stringify(resp, null, "  ") );
+      hljs.highlightBlock(e);
+    });
+  }
+
+  $('.live_requests').waypoint(function() {
+    makeRequest(this);
+  }, {
+    offset: function() { return $(window).height(); }
+  });
+});
