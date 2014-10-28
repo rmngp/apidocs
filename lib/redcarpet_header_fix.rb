7b076c2d6e881577e10ad8661b1003fede08c321
klacointe <kevinlacointe@gmail.com>
2014-10-28 14:19:13 +0100

    Fix toc ids uniqueness.

--- lib/redcarpet_header_fix.rb
+++ lib/redcarpet_header_fix.rb
@@ -1,7 +1,15 @@
 module RedcarpetHeaderFix
+  @@ids = {}
+
   def header(text, level, id)
     clean_id = id.gsub(/[\.]/, '-').gsub(/[^a-zA-Z0-9\-_]/, '')
-    "<h#{level} id='#{clean_id}'>#{text}</h1>"
+    if @@ids[clean_id].nil?
+      @@ids[clean_id] = 0
+    else
+      @@ids[clean_id] += 1
+      clean_id = "#{clean_id}-#{@@ids[clean_id]}"
+    end
+    "<h#{level} id='#{clean_id}'>#{text}</h#{level}>"
   end
 end
 
